39559dcb00cd2c97d3f29700a474f826f6193107
Michael Klein <michaelklein@Michaels-MacBook-Pro.local>
2020-07-25 14:28:27 -0400

    initial commit of files

---
+++ Calculator/calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Michael Klein on 7/25/20.
+//  Copyright © 2020 Michael Klein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
